fd17ff915b80d1b311fced8a24e7326504538085
kngreek <konstantinos3500@gmail.com>
2022-07-15 12:48:02 +0300

    etoimo to deck!

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const suits = ["S", "C", "H", "D"];
 
 let values = [];
 
-for (let i = 0; i <= 13; i++) {
+for (let i = 1; i <= 13; i++) {
   values.push(i);
 }
-values.forEach((e) => console.log(e));
 
-function getDeck() {
-  let deck = new Array();
-
-  for (let i = 0; i < suits.length; i++) {
-    for (let x = 0; x < values.length; x++) {
-      let card = { Value: values[x], Suit: suits[i] };
-      deck.push(card);
+function crateDeck() {
+  const deck = [];
+  for (let s = 0; s < suits.length; s++) {
+    for (let v = 0; v < values.length; v++) {
+      const value = values[v];
+      const suit = suits[s];
+      deck.push(value + suit);
     }
   }
+
+  return deck;
 }
+console.log(crateDeck());
