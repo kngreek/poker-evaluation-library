19f0a6dc4bb7af92a8960f23183ef0bb3cc885c7
kngreek <konstantinos3500@gmail.com>
2022-07-22 11:50:24 +0300

    attempt for power

--- index.js
+++ index.js
@@ -46,20 +46,43 @@ const hand = draw(shuffledDeck);
 console.log(shuffledDeck);
 console.log(draw(shuffledDeck));
 console.log(shuffledDeck);
-function numberPower(hand) {
-  numberOfHand = [];
+function numberHand(hand) {
+  const numberOfHand = [];
   for (i = 0; i < 5; i++) {
     numberOfHand[i] = hand[i].replace(/\D/g, "");
   }
   return numberOfHand;
 }
-console.log(numberPower(hand));
+const numberOfHand = numberHand(hand);
+console.log(numberHand(hand));
 
-function suitPower(hand) {
-  suitsOfHand = [];
+function suitHand(hand) {
+  const suitsOfHand = [];
   for (i = 0; i < 5; i++) {
     suitsOfHand[i] = hand[i].replace(/[0-9]/g, "");
   }
   return suitsOfHand;
 }
-console.log(suitPower(hand));
+const suitsOfHand = suitHand(hand);
+console.log(suitHand(hand));
+
+let power = 0;
+function highCard(numberHand, power) {
+  for (i = 0; i < 5; i++) {
+    if (numberHand[i] > 10) {
+      power = 1;
+    }
+  }
+  return power;
+}
+console.log(highCard(numberHand, power));
+
+function pair(numberHand) {
+  const count = {};
+
+  numberHand.forEach((element) => {
+    count[element] = (count[element] || 0) + 1;
+  });
+  return count;
+}
+console.log(pair(numberHand));
